b064459899641a55707659d65bbef5895b8ef2af
unknown <dreicleal@hotmail.com>
2025-02-18 08:48:09 +0000

    included clear chat history feature
    New clear chat history feature

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -5,8 +5,9 @@ import { useEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 export default function Chat() {
-  const { messages, sendMessage, loading } = useChat();
+  const { messages, sendMessage, loading, clearChat } = useChat();
   const [input, setInput] = useState("");
+  const [modal, setModal] = useState(false);
   const messageEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -19,16 +20,28 @@ export default function Chat() {
     setInput("");
   };
 
+  const confirm = () => {
+    clearChat()
+    setModal(false)
+  }
+
   return (
-    <div className="max-w-lg mx-auto p-4">
+    <div className="relative max-w-lg mx-auto p-4">
+      {modal && <div className="absolute top-20 left-10 bg-accentActive w-[350px] rounded-md p-8">
+        <h2 className="font-bold p-2">Tem certeza que deseja limpar o histórico da conversa?</h2>
+        <div className="flex justify-evenly">
+
+        <button className="px-2 w-[40%] rounded-md bg-secondary hover:bg-accentHover" onClick={()=> setModal(false)}>Cancelar</button><button className="px-2 w-[40%] rounded-md bg-secondary hover:bg-accentHover" onClick={confirm}>Apagar</button>
+        </div>
+        </div>}
       <div className="border rounded-lg p-2 flex flex-col h-96 overflow-y-auto bg-background">
         {messages.map((msg, index) => (
           <div
             key={index}
             className={`p-2 my-1 rounded-md w-fit max-w-[95%] ${
               msg.role === "user"
-                ? "bg-blue-500 text-white self-end"
-                : "bg-gray-500"
+                ? "bg-primary text-white self-end"
+                : "bg-secondary"
             }`}
           >
             <ReactMarkdown
@@ -48,14 +61,14 @@ export default function Chat() {
           </div>
         ))}
         <div ref={messageEndRef} />
-        {loading && <p className="text-gray-500">Thinking...</p>}
+        {loading && <p className="text-secondary">Thinking...</p>}
       </div>
       <div className="flex mt-2">
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          className="border p-2 flex-1 rounded-lg text-gray-500"
+          className="border p-2 flex-1 rounded-lg text-secondary outline-none"
           placeholder="Type a message..."
         />
         <button
@@ -65,6 +78,12 @@ export default function Chat() {
           Send
         </button>
       </div>
+      <button
+        className="bg-secondary p-2 hover:bg-secondaryHover active:bg-accentActive mt-1"
+        onClick={() => setModal(true)}
+      >
+        Clear chat history
+      </button>
     </div>
   );
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="bg-primary p-2">
+    <div className="bg-secondary p-2">
       <Link href={"/"}>
         <Image className="w-10 h-10" src={logo} alt="bot logo" />
       </Link>
