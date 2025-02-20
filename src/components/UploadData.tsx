fbb9340154a01054c496256a571c90ca9856a8c1
unknown <dreicleal@hotmail.com>
2025-02-20 14:30:48 +0000

    upload data style updated
    styling upload data component

--- src/components/UploadData.tsx
+++ src/components/UploadData.tsx
@@ -1,55 +1,24 @@
 "use client";
-import * as XLSX from "xlsx";
-import { v4 as uuidv4 } from "uuid";
-import { ChangeEvent, useState } from "react";
-import { IDeck } from "@/context/flashCardsContext/flashCardsContextTypes";
-import ConfirmModal from "./ConfirmModal";
 
-type IFile = {
-  term: string;
-  definition: string;
-};
+import { useFlashCards } from "@/context/flashCardsContext/flashcardsGameContext";
 
 export default function UploadData() {
-  const [newDeckData, setNewDeckData] = useState<IDeck | null>(null);
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
-    setModalOpen(true);
-    const file = event.target.files?.[0];
-    if (!file) return;
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      if (e.target?.result) return;
-      const data = new Uint8Array(e.target?.result as ArrayBuffer);
-      const workbook = XLSX.read(data, { type: "array" });
-      const sheet = workbook.Sheets[workbook.SheetNames[0]];
-      const jsonData: IFile[] = XLSX.utils.sheet_to_json(sheet);
-      const newDeck = {
-        id: uuidv4(),
-        name: "New Deck",
-        cards: jsonData.map((row) => ({
-          id: uuidv4(),
-          term: row.term,
-          definition: row.definition,
-        })),
-      };
-      console.log(newDeck);
-      setNewDeckData(newDeck);
-    };
-    reader.readAsArrayBuffer(file);
-  };
+  const { handleFileUpload } = useFlashCards();
 
   return (
-    <div>
-      <input type="file" accept=".xlsx" onChange={(e) => handleFileUpload(e)} />
-      {modalOpen && (
-        <ConfirmModal
-          newDeckData={newDeckData}
-          setModalOpen={setModalOpen}
-          setNewDeckData={setNewDeckData}
-        />
-      )}
-    </div>
+    <label
+    htmlFor="selection"
+    className="relative w-[180px] h-[120px] rounded-md p-2 flex flex-col items-center justify-center hover:bg-white/25 transition duration-300 cursor-pointer"
+  >
+
+    <p className="absolute top-[-7%] text-primary text-9xl font-semibold h-[75px]">+</p>
+    <input
+      type="file"
+      className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+      id="selection"
+      accept=".xlsx"
+      onChange={(e) => handleFileUpload(e)}
+    />
+  </label>
   );
 }
